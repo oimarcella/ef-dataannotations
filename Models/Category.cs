69d6c52368d05bbba4097769387ccb8af7e30024
Marcella Amorim <devabefarma4@outlook.com>
2024-06-19 13:23:32 -0300

    add: data annotations para coluna id

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models
 {
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
