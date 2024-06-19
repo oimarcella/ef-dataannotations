c9d4621ac339c0bc5bfb1e32137c023c913505c2
Marcella Amorim <devabefarma4@outlook.com>
2024-06-19 15:43:28 -0300

    add: navigation properties para category e author na tabela post

--- Models/Category.cs
+++ Models/Category.cs
@@ -3,12 +3,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Models
 {
+    [Table("Caegory")]
     public class Category
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)] // não existe essa validacao direto pelo banco, mas podemos fazer com data annotations
+        [MaxLength(80)]
+        [Column("Name", TypeName="NVARCHAR")]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        [Column("Slug", TypeName="VARCHAR")]
         public string Slug { get; set; }
     }
 }
